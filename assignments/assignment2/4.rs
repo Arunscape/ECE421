6ce3f1d4e17d0550c2021ac6d31bc28a32fa42e0
Arunscape <8227297+Arunscape@users.noreply.github.com>
2020-02-02 20:36:59 -0700

    I submitted it

--- assignments/assignment2/4.rs
+++ assignments/assignment2/4.rs
@@ -14,7 +14,6 @@ fn main() {
     }
     { 
         let vec1_2=vec![vec1, vec2];
-    println!("{}", size_of_val(&vec1_2));
         let vec_flattened = vec1_2.iter().flatten(); 
         println!("size of vec_flattened = {} bytes", size_of_val(&vec_flattened));
     }
