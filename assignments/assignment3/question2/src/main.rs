778ba9ae9f345e9ad3029fbb4de7e550e7bbe36a
Arunscape <8227297+Arunscape@users.noreply.github.com>
2020-02-05 17:45:59 -0700

    q1 lab 2

--- assignments/assignment3/question2/src/main.rs
+++ assignments/assignment3/question2/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+}
+
+struct Player {
+    id: i32,
+    first_name: String,
+    last_name: String,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use hamcrest2::prelude::*;
+
+    #[test]
+    fn test_Player(){
+    }
 }
